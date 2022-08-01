bb8c2b90aaa6107ddc2868b8f869b286728d563e
Tuvshinbayar Oyuntsetseg <tuvshinbayar.o@Tuvshinbayars-MacBook-Pro.local>
2022-08-01 09:04:48 +0800

    fixed video

--- src/components/Video.js
+++ src/components/Video.js
@@ -8,8 +8,7 @@ const Video = () => {
         </div>
         <h1 className='text-red-500 text-4xl font-bold w-1/2 text-center'>Have <span className='text-white'>a nice for Weekend</span></h1>
         <div className='pt-5 px-2'>
-            {/* <iframe width="390" height="240" src="https://www.youtube.com/embed/8G_ORAIztU4?list=TLGGMfBJMdiTVMAyMjA3MjAyMg" title="Mobi festival last" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe> */}
-            <iframe width="390" height="240" src="https://www.youtube.com/embed/8G_ORAIztU4?list=TLGGMfBJMdiTVMAyNTA3MjAyMg" title="Mobi festival last" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          <iframe width="320" height="240" src="https://www.youtube.com/embed/8G_ORAIztU4?list=TLGGMfBJMdiTVMAwMTA4MjAyMg" title="Mobi festival last" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
         </div>
     </div>
   )
