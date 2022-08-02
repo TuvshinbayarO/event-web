25ece34c5f0850d02eed20cfa4fffd0bf8387eaf
tuvshinbayaro <tuvshinbayar.o@mobicom.mn>
2022-08-02 17:54:19 +0800

    fixed typo

--- src/components/Artist.js
+++ src/components/Artist.js
@@ -11,7 +11,7 @@ function Artist() {
         <h1 className='text-red-500 text-4xl font-bold w-1/2 text-center'>Artist <span className='text-white'></span></h1>
         <div className='flex flex-col justify-center items-center text-center pt-5'>
             {/* <img alt='Alt' className='w-[95%] rounded-lg' src={Artists} /> */}
-            <h1 className='flex flex-col justify-center items-center p-10 font-extrabold text-transparent text-4xl bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600 space-y-5 w-[80%] outline outline-offset-2 outline-1 outline-pink-500 rounded-lg'>Comming Soon</h1>
+            <h1 className='flex flex-col justify-center items-center p-10 font-extrabold text-transparent text-4xl bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600 space-y-5 w-[80%] outline outline-offset-2 outline-1 outline-pink-500 rounded-lg'>Coming Soon</h1>
         </div>
     </div>
   )
